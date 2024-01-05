5d7aa3e165a4401a554d00018a03b2fc5c72f0ae
joserivera <joserivera@ASUS4070>
2024-01-05 18:11:25 -0400

    Add project files.

---
+++ HoteListing.API/Program.cs
@@ -0,0 +1,56 @@
+// IoC container. This allows me to use things like dependency injection eventually.
+// More services can be introduced later
+
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// If a Cors policy isnt used, when we run our API it would only be accesible in the same server where it's running
+// This would defeat the purpose of hosting the API in the internet
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("AllowAll",
+        b => b.AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod());
+
+});
+
+// I want to use Serilog, Serilog Sinks and Expressions
+
+// I want to use Serilog, I create an instance of the builder (ctx), and the logger configuration (lc)
+// I ask the logger to Write to console, and read from the builder's Configuration (appsettings.json).
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// This is oging to add default request logging. 
+// Log type of requests comming in and how long they took to respond.
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+//Uncomment the line below to enable use of the API with Functions
+
+//app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
